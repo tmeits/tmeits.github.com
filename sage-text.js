dea0b7880f3897dfd528e636c870a15c5c0e7d08
tmeits <tmeits@gmail.com>
2015-06-04 15:43:18 +0600

    Update sage-text.js

--- sage-text.js
+++ sage-text.js
@@ -1,11 +1,8 @@
 function sageFooter() {
-	document.write('<div class="footer"><p>Sage is free, open-source math - ' +
-		'learn more and install at <a href="http://sagemath.org">sagemath.org</a>, ' +
-		'and try it out on the <a href="http://sagenb.org">Notebook Server</a><br />' +
-		'&copy <a href="http://www.brandoncurtis.com/">Brandon Curtis</a>, ' +
-		'2012-<script>document.write(new Date().getFullYear())</script><noscript>2015</noscript> - ' +
-		'this work is licensed under a <a href="http://creativecommons.org/licenses/by-sa/3.0/">' +
-		'Creative Commons Attribution-ShareAlike 3.0 License</a></p></div>')
+	document.write('<div class="footer"><p>' +
+		'&copy <a href="http://www.com/">..</a>, ' +
+		'2012-<script>document.write(new Date().getFullYear())</script><noscript>2015</noscript> ' +
+		'</p></div>')
 	}
 
 function sageHome() {
