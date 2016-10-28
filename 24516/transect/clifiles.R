d49455c82ae508574ccdef9dc67a4c1b28f11ca2
tmeits <tmeits@gmail.com>
2016-10-28 16:02:22 +0700

    del function all script

--- 24516/transect/clifiles.R
+++ 24516/transect/clifiles.R
@@ -1,16 +1,56 @@
-# downloadable list of files containing climatic measurements
-# the first five characters in the file name represent the code of the weather station
-# http://stackoverflow.com/questions/4876813/using-r-to-list-all-files-with-a-specified-extension
-readCliFiles <- function() {
-    altai.files <- list.files(pattern = "\\.txt$", path = "cli/altai", ignore.case = TRUE,
-        recursive = FALSE, full.names = FALSE)
-    lena.files <- list.files(pattern = "\\.txt$", path = "cli/lena", ignore.case = TRUE)
-    north.files <- list.files(pattern = "\\.txt$", path = "cli/north", ignore.case = TRUE)
-    yenisei.files <- list.files(pattern = "\\.txt$", path = "cli/yenisei",
-        ignore.case = TRUE)
+# ===============================================================================
+# Name : downloadable list of files containing climatic measurement Author :
+# tmeits Date : 26.10.2016
+# ===============================================================================
+source("http://tmeits.github.io/24516/transect/setdw.R")
 
-    all.files <- sort(c(altai.files, lena.files, north.files, yenisei.files))
-    return(all.files)
+# 
+getStationCode <- function(listFiles) {
+    len <- length(listFiles)
+    if (len != 0) {
+        listCodes <- c(1:len)
+        for (i in listCodes) {
+            listCodes[i] <- as.numeric(substr(listFiles[i], 1, 5))
+        }
+    } else stop("An empty list of files!")
+    
+    return(as.numeric(listCodes))
 }
-#print(readCliFiles())
-all.files <- readCliFiles()
+
+alati.path <- "cli/altai"
+altai.files <- list.files(pattern = "\\.txt$", path = alati.path, ignore.case = TRUE, 
+    recursive = FALSE, full.names = FALSE)
+alati.code <- getStationCode(altai.files)
+
+lena.path <- "cli/lena"
+lena.files <- list.files(pattern = "\\.txt$", path = lena.path, ignore.case = TRUE)
+lena.code <- getStationCode(lena.files)
+
+north.path <- "cli/north"
+north.files <- list.files(pattern = "\\.txt$", path = north.path, ignore.case = TRUE)
+north.code <- getStationCode(north.files)
+
+yenisei.path <- "cli/yenisei"
+yenisei.files <- list.files(pattern = "\\.txt$", path = yenisei.path, ignore.case = TRUE)
+yenisei.code <- getStationCode(yenisei.files)
+
+aisori.path <- "cli/aisori"
+aisori.files <- list.files(pattern = "\\.txt$", path = aisori.path, ignore.case = TRUE)
+aisori.code <- getStationCode(aisori.files)
+
+all.path <- c(alati.path, lena.path, north.path, yenisei.path, aisori.path)
+all.files <- c(altai.files, lena.files, north.files, yenisei.files, aisori.files)
+all.code <- getStationCode(all.files)
+
+
+
+
+
+
+
+
+
+
+
+
+
