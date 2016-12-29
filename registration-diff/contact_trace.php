c913b8aabc6521c98a16ad43cecc480f3c1b3c1b
tmeits <tmeits@gmail.com>
2016-12-29 17:55:04 +0700

    Add files via upload

--- registration-diff/contact_trace.php
+++ registration-diff/contact_trace.php
@@ -1,7 +1,8 @@
 <?php
-$recipient_email    = "ilynva@gmail.com"; //recepient
-$from_email         = "ilyna.lora@gmail.com"; //from email using site domain.
-
+//$recipient_email    = "ilynva@gmail.com"; //recepient
+$recipient_email    = "ilynva@yandex.ru"; //recepient
+//$from_email         = "ilyna.lora@gmail.com"; //from email using site domain.
+$from_email         = "ilynva@gmail.com";
 
 if(!isset($_SERVER['HTTP_X_REQUESTED_WITH']) AND strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) != 'xmlhttprequest') {
 	die('Sorry Request must be Ajax POST'); //exit script
@@ -9,13 +10,30 @@ if(!isset($_SERVER['HTTP_X_REQUESTED_WITH']) AND strtolower($_SERVER['HTTP_X_REQ
 
 if($_POST){
     
+    // capture and transform data from the form
     $sender_name 	= filter_var($_POST["name"], FILTER_SANITIZE_STRING); //capture sender name
     $sender_email 	= filter_var($_POST["email"], FILTER_SANITIZE_STRING); //capture sender email
     $country_code   = filter_var($_POST["phone1"], FILTER_SANITIZE_NUMBER_INT);
     $phone_number   = filter_var($_POST["phone2"], FILTER_SANITIZE_NUMBER_INT);
-    $subject        = filter_var($_POST["subject"], FILTER_SANITIZE_STRING);
+//    $subject        = filter_var($_POST["subject"], FILTER_SANITIZE_STRING);
+    $subject = "TRACE 2017 RegForm";
     $message 		= filter_var($_POST["message"], FILTER_SANITIZE_STRING); //capture message
-
+    //
+    $sender_lastname = filter_var($_POST["lastname"], FILTER_SANITIZE_STRING);
+    $sender_addressing = filter_var($_POST["addressing"], FILTER_SANITIZE_STRING);
+    $sender_country = filter_var($_POST["country"], FILTER_SANITIZE_STRING);
+//    $sender_ = filter_var($_POST[""], FILTER_SANITIZE_STRING);
+    $sender_affiliation = filter_var($_POST["affiliation"], FILTER_SANITIZE_STRING);
+    $sender_position = filter_var($_POST["position"], FILTER_SANITIZE_STRING);
+    $sender_presentation = filter_var($_POST["presentation"], FILTER_SANITIZE_STRING);
+    $sender_message = filter_var($_POST["message"], FILTER_SANITIZE_STRING);
+    $sender_title = filter_var($_POST["title"], FILTER_SANITIZE_STRING);
+    $sender_visa = filter_var($_POST["visa"], FILTER_SANITIZE_STRING);
+    $sender_may16 = filter_var($_POST["may16"], FILTER_SANITIZE_STRING);
+    $sender_may17 = filter_var($_POST["may17"], FILTER_SANITIZE_STRING);
+    $sender_excursioncheckbox = filter_var($_POST["excursioncheckbox"], FILTER_SANITIZE_STRING);
+    $sender_dinner = filter_var($_POST["dinner"], FILTER_SANITIZE_STRING);
+    //
     $attachments = $_FILES['file_attach'];
     
 	
@@ -36,15 +54,18 @@ if($_POST){
         print json_encode(array('type'=>'error', 'text' => 'Enter only digits in phone number'));
         exit;
     }
-    if(strlen($subject)<3){ //check emtpy subject
+    /*if(strlen($subject)<3){ //check emtpy subject
         print json_encode(array('type'=>'error', 'text' => 'Subject is required'));
         exit;
-    }
+    }*/
     if(strlen($message)<3){ //check emtpy message
-        print json_encode(array('type'=>'error', 'text' => 'Too short message! Please enter something.'));
+        print json_encode(array('type'=>'error', 'text' => 'Too short Abstract Submission Message! Please enter something.'));
         exit;
     }
 
+    
+    
+    
     
     $file_count = count($attachments['name']); //count total files attached
     $boundary = md5("sanwebe.com"); 
@@ -60,8 +81,33 @@ if($_POST){
         $body = "--$boundary\r\n";
         $body .= "Content-Type: text/plain; charset=ISO-8859-1\r\n";
         $body .= "Content-Transfer-Encoding: base64\r\n\r\n"; 
-//        $body .= chunk_split(base64_encode($message)); 
-        $form_body = $message."......".$sender_name;
+//        $body .= chunk_split(base64_encode($message));
+        // Writing your letter by filling out the form 
+        $form_body  = "TRACE2017 Registration Form\r\n"; 
+        $form_body .= "===============================\r\n";
+        $form_body .= "First name: ".$sender_name."\r\n";
+        $form_body .= "Last name: ".$sender_lastname."\r\n";
+        $form_body .= "Form of addressing: ".$sender_addressing."\r\n";
+        $form_body .= "Country: ".$sender_country."\r\n";
+        $form_body .= "Affiliation: ".$sender_affiliation."\r\n";
+        $form_body .= "Position: ".$sender_position."\r\n";
+        $form_body .= "Type of presentation: ".$sender_presentation."\r\n";
+        $form_body .= "Presentation Title: ".$sender_title."\r\n";
+        $form_body .= "Abstract Submission Message: ".$sender_message."\r\n";
+        $form_body .= "Email adress: ".$sender_email."\r\n";
+        $form_body .= "Phone number: ".$country_code."-".$phone_number."\r\n";
+        $form_body .= "Do you need visa: ".$sender_visa."\r\n";
+        $form_body .= "Short courses, May 16: ".$sender_may16."\r\n";
+        $form_body .= "Short courses, May 17: ".$sender_may17."\r\n";
+        if($sender_excursioncheckbox == ""){
+            $form_body .= "Excursion Kaliningrad and Baltic Federal University: No\r\n";
+        }else{
+            $form_body .= "Excursion Kaliningrad and Baltic Federal University: Yes\r\n";
+        }
+        $form_body .= "Conference dinner: ".$sender_dinner."\r\n";
+//        $form_body .= ": ".."\r\n";
+        $form_body .= "===============================\r\n";
+        //
         $body .= chunk_split(base64_encode($form_body));
         //attachments
         for ($x = 0; $x < $file_count; $x++){       
@@ -109,12 +155,13 @@ if($_POST){
     $sentMail = mail($recipient_email, $subject, $body, $headers);
     if($sentMail) //output success or failure messages
     {       
-        print json_encode(array('type'=>'done', 'text' => 'Thank you for your registration'));
+        print json_encode(array('type'=>'done', 'text' => 'Thank you for your registration. To print forms, press Ctr-P.'));
 		exit;
     }else{
 //        print json_encode(array('type'=>'error', 'text' => 'Could not send mail! Please check your PHP mail configuration.'));
         print json_encode(array('type'=>'error', 'text' => 'Could not send registration! Please send e-mail: TRACE2017russia@mail.ru.'));
 		exit;
     }
+    //
 }
 ?>
--- registration-diff/index.php
+++ registration-diff/index.php
@@ -12,45 +12,54 @@
 <div class="form-style-logo"></div>
 <div class="form-style" id="contact_form">
     <div class="form-style-heading">Registration Form</div>
+    
+    Before to start the registration procedure please  prepare your abstarct fitting the <a href="http://trace2017.sfu-kras.ru/docs/trace_2017_abstract_template.docx" style="color:#801212;"><u><b> abstract template </b></u> </a> and if you need visa prepare the pdf copy of your valid international passport and filled 
+    
+    <a href="http://trace2017.sfu-kras.ru/docs/Visa_invitation_application_form-2.docx" style="color:#801212;"><u><b>visa-invitation form.</b></u></a>
+    
+    
+    
+    <div class="form-style-heading3"></div>
     <div id="contact_results"></div>
     <form id="contact_body" method="post" action="contact_trace.php">
         <label for="name"><span>First name <span class="required">*</span></span>
             <input type="text" name="name" placeholder="First name" value="First name" data-required="true"/>
         </label>
-        <label for="name"><span>Last name <span class="required">*</span></span>
+        <label for="lastname"><span>Last name <span class="required">*</span></span>
             <input type="text" name="lastname" placeholder="Last name" value="Last name" data-required="true"/>
         </label>
         <!-- Form of addressing -->
-        <label for="name"><span>Form of addressing<span class="required">*</span></span>
-            <input type="radio" name="addressing" value="dr"  checked="checked"/> Dr.
-            <input type="radio" name="addressing" value="prof"/> Prof. 
-            <input type="radio" name="addressing" value="mr"/> Mr 
-            <input type="radio" name="addressing" value="mrs"/> Mrs <br/>
+        <label for="addressing"><span>Form of addressing<span class="required">*</span></span>
+            <input type="radio" name="addressing" value="Dr."  checked="checked"/> Dr.
+            <input type="radio" name="addressing" value="Prof."/> Prof. 
+            <input type="radio" name="addressing" value="Mr"/> Mr 
+            <input type="radio" name="addressing" value="Mrs"/> Mrs <br/>
            
         </label><br>
         <!-- Form of addressing -->
-        <label for="name"><span>Country<span class="required">*</span></span>
+        <label for="country"><span>Country<span class="required">*</span></span>
             <input type="text" name="country" placeholder="Country" value="Country"  data-required="true"/>
         </label>
-        <label for="name"><span>Affiliation<span class="required">*</span></span>
+        <label for="affiliation"><span>Affiliation<span class="required">*</span></span>
             <input type="text" name="affiliation"  placeholder="Affiliation" value="Affiliation" data-required="true"/>
         </label>
-        <label for="name"><span>Position<span class="required">*</span></span>
+        <label for="position"><span>Position<span class="required">*</span></span>
             <input type="text" name="position"  placeholder="Position" value="Position" data-required="true"/>
         </label>
         
-        <label for="name"><span>Type of presentation<span class="required">*</span></span>
-            <input type="radio" name="presentation" value="oral"  checked="checked"/> Oral
-            <input type="radio" name="presentation" value="poster"/> Poster 
+        <label for="presentation"><span>Type of presentation<span class="required">*</span></span>
+            <input type="radio" name="presentation" value="Oral"  checked="checked"/> Oral
+            <input type="radio" name="presentation" value="Poster"/> Poster 
         </label><br>
         
+        <label for="title"><span>Presentation Title<span class="required">*</span></span>
+            <input type="text" name="title"  placeholder="Title" value="Title" data-required="true"/>
+        </label>
+        
         <label for="message"><span>Abstract Submission Message <span class="required">*</span></span>
-            <textarea name="message" data-required="true"></textarea>
+            <textarea name="message"   placeholder="Message" value="Message..." data-required="true"></textarea>
         </label>
 
-        <label for="name"><span>Presentation Title<span class="required">*</span></span>
-            <input type="text" name="position"  placeholder="Title" value="Title" data-required="true"/>
-        </label>
         
         <label><span>Abstract Submission</span>
             <input type="file" name="file_attach[]"  />
@@ -64,50 +73,58 @@
             <input type="text" name="phone2" maxlength="15" placeholder="2062463"  value="2062463" data-required="true"/>
         </label>
         
-        <label for="name"><span>Do you need visa<span class="required">*</span></span>
-            <input type="radio" name="visa" value="yes"  checked="checked"/> Yes
-            <input type="radio" name="visa" value="no"/> No 
+        <label for="visa"><span>Do you need visa<span class="required">*</span></span>
+            <input type="radio" name="visa" value="Yes"  checked="checked"/> Yes
+            <input type="radio" name="visa" value="No"/> No 
         </label><br>
         
         <label><span>Fill the form</span>
             <input type="file" name="file_attach[]"  />
         </label>
         
-        <label><span>Passport scan</span>
-            <input type="file" name="file_attach[]"  />
+        <label><span>Passport scan *</span>
+<!--            <input type="file" name="file_attach[]" data-required="true"/>-->
+            <input type="file" name="file_attach[]" />
         </label>
         
         <!-- <b>Short introduction and demonstration courses</b><hr><br> -->
         <div class="form-style-heading2">Short introduction and demonstration courses</div>
         
-        <label for="may16"><span>May 16: check one</span>
+        <label for="may16"><span>May 16: 30 Euros</span>
             <select name="may16">
-            <option value="Select the course, each worth 30 Euro">Select the course, each worth 30 Euro</option>
+<!--            <option value="Select the course, each worth 30 Euro">Select the course, each worth 30 Euro</option>-->
             <option value="R course (Ernst van der Maaten)">R course (Ernst van der Maaten)</option>
             <option value="Climate-growth analyses (Wolfgang Beck)">Climate-growth analyses (Wolfgang Beck)</option>
             </select>
         </label>
         
-        <label for="may17"><span>May 17: check one</span>
+        <label for="may17"><span>May 17: 30 Euros</span>
             <select name="may17">
-            <option value="Select the course, each worth 30 Euro">Select the course, each worth 30 Euro</option>
+<!--            <option value="Select the course, each worth 30 Euro">Select the course, each worth 30 Euro</option>-->
             <option value="Methods of quantitative and functional wood anatomy (Patrick Fonti)">Methods of quantitative and functional wood anatomy (Patrick Fonti)</option>
             <option value="Process modeling of tree-ring season growth">Process modeling of tree-ring season growth</option>
             </select>
         </label>
         <div class="form-style-heading3"></div>
-        <label for="name"><span>Excursion to Kaliningrad and Baltic Federal University (20 Euros)<span class="required">*</span></span>
+        <!--<label for="name"><span>Excursion to Kaliningrad and Baltic Federal University (20 Euros)<span class="required">*</span></span>
             <input type="radio" name="excursion" value="yes"  checked="checked"/> Yes
             <input type="radio" name="excursion" value="no"/> No 
-        </label><br><br><br>
-        <label for="name"><span>Conference dinner (30 Euros) <span class="required">*</span></span>
-            <input type="radio" name="dinner" value="yes"  checked="checked"/> Yes
-            <input type="radio" name="dinner" value="no"/> No 
-        </label><br><br>
+        </label><br><br><br>-->
         
-        <label for="name"><span>Sesions, coffee breaks, welkome party ... (150 Euros) <span class="required">*</span></span>
-            <input checked disabled type="checkbox" name="" value="150">you can not refuse (150 Euros) <br>
-                <span>Sesions, coffee breaks, welkome party</span></p>
+        <label for="excursioncheckbox"><span>Excursion (20 Euros) <span class="required">*</span></span>
+            <input checked type="checkbox" name="excursioncheckbox" value="20"> Kaliningrad and Baltic Federal University
+<!--            <span>Excursion to Kaliningrad and Baltic Federal University</span>-->
+        </label>
+        
+        <label for="dinner"><span>Conference dinner (30 Euros) <span class="required">*</span></span>
+            <input type="radio" name="dinner" value="Yes"  checked="checked"/> Yes
+            <input type="radio" name="dinner" value="No"/> No 
+        </label><br>
+        
+        <label for="sesions"><span>Sesions, coffee breaks, welkome party ... <span class="required">*</span></span>
+            <input checked disabled type="checkbox" name="sesions" value="150">You can not refuse (150 Euros) 
+<!--            <br>-->
+<!--                <span>Sesions, coffee breaks, welkome party</span>-->
         </label><br><br>
         <!-- <label for="subject"><span>Regarding</span>
             <select name="subject">
@@ -116,9 +133,9 @@
             <option value="Partnership">Partnership Oppertunity</option>
             </select>
         </label> -->
-        <br><br>
+        
         <div class="form-style-heading2">Conference fees</div>
-        <b>Cost: 150 Euros</b>
+        <b>Cost: 260 Euros</b>
         <div class="form-style-heading3"></div>
         <label><span>&nbsp;</span>
         	<button type="submit">Submit</button>
@@ -127,7 +144,8 @@
 </div>
 
 
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
+<!--<script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>-->
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
 <script type="text/javascript">
 var allowed_file_size = "1048576";
 var allowed_files = ['image/png', 'image/gif', 'image/jpeg', 'image/pjpeg'];
